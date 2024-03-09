6d0c99df073bd09528b5e65b697a917105979ff5
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-03-09 10:51:08 -0400

    Initialize the app's assets
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -8,6 +8,8 @@ import Footer from '../footer/footer.component.jsx';
 function App() {
   return (
     <>
+    <span className='md-icon'>home</span>
+    <a href='https://music.youtube.com/' target='_blank'>Im a link!</a>
       <Header />
       <Nav />
       <Main />
